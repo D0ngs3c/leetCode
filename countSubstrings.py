c57362aa0bc70dd2b0e15663aacc7b3067558127
dongdxu <dongdxu.vip@foxmail.com>
2018-07-06 15:45:30 +0800

    upload

--- countSubstrings.py
+++ countSubstrings.py
@@ -25,43 +25,24 @@ The input string length won't exceed 1000.
 注意回文字符串有奇数和偶数两种形式，如果是奇数长度，那么i位置就是中间那个字符的位置，所以我们左右两遍都从i开始遍历；
 如果是偶数长度的，那么i是最中间两个字符的左边那个，右边那个就是i+1，这样就能cover所有的情况啦，而且都是不同的回文子字符串
 '''
-class Solution():
+
+class Solution(object):
+    # def __init__(self):
+    #     self.res = 0
     def countSubstrings(self, str):
         if not str:
             return 0
-        res = 0
+        self.res = 0
         for i in range(len(str)):
-            j = i
-            while  (i >= 0 and j < len(str) and str[i] == str[j]):
-                i -= 1
-                j += 1
-                res += 1
+            self.helper(str, i, i, self.res)
+            self.helper(str, i, i + 1, self.res)
+        return self.res
 
-        for i in range(len(str)):
-            j = i + 1
-            while  (i >= 0 and j < len(str) and str[i] == str[j]):
-                i -= 1
-                j += 1
-                res += 1
-        return res
-
-# class Solution2(object):
-#     # def __init__(self):
-#     #     self.res = 0
-#     def countSubstrings(self, str):
-#         if not str:
-#             return 0
-#         res = []
-#         for i in range(len(str)):
-#             self.helper(str, i, i, res)
-#             self.helper(str, i, i + 1, res)
-#         return len(res)
-
-#     def helper(self, str, i, j, res):
-#         while(i >= 0 and j < len(str) and str[i] == str[j]):
-#             i -= 1 
-#             j += 1
-#             res.append(1)  
+    def helper(self, str, i, j, res):
+        while(i >= 0 and j < len(str) and str[i] == str[j]):
+            i -= 1 
+            j += 1
+            self.res += 1 
 
 
 # Time complexity O(n)
